1804d9e1d750dce85719950f3d7719f3a808b624
流光 <wyyang9688@qq.com>
2024-07-16 16:03:21 +0800

    save

--- src/common/route.ts
+++ src/common/route.ts
@@ -3,19 +3,19 @@ import router from "@/router/index";
 export {};
 
 declare module "vue-router" {
-  interface RouteMeta {
-    transition?: string;
-    isUnKeep?: boolean;
-  }
+    interface RouteMeta {
+        transition?: string;
+        isUnKeep?: boolean;
+    }
 }
 
 function useRT() {
-  const push = (op: any) => {
-    console.log(router);
-    return router.push(op);
-  };
+    const push = (op: any) => {
+        console.log(router);
+        return router.push(op);
+    };
 
-  return { push };
+    return { push };
 }
 
 export const rt = useRT;
